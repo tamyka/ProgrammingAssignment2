a8531a3dc2dcd1f616f461f2d0db8c3d80971786
Tamyka Bell <tamyka.bell@gmail.com>
2014-09-21 23:25:01 +1000

    First attempt at committing and I got stuck in vim. Function looks ok

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This pair of functions caches the inverse of a matrix to speed up computation
+## time rather than recalculating. It is created as an R Programming assignment.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(i) inv <<- i
+        getinv <- function() inv
+        list(set = set, 
+             get = get,
+             setinv = setinv,
+             getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated (and the 
+## matrix has not changed), then the cachesolve should retrieve the inverse 
+## from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)){
+                message("getting cached inverse value")
+                return(m)
+        } else {
+                data <- x$get()
+                inv <- solve(data, ...)
+                x$setinv(inv)
+                inv
+        }
 }
